41ae8ff7b2d40d1bbd2007421b532df21429eb12
wenshibiao <wenshibiao@outlook.com>
2025-01-24 14:08:39 +0800

    edit _worker.js

--- _worker.js
+++ _worker.js
@@ -2,9 +2,15 @@ export default {
     async fetch(request, env) {
         const url = new URL(request.url);
         if (url.pathname.startsWith('/api/')) {
-            return new Response('Ok');
-        }
 
+            return new Response('Ok');
+        } else if (url.pathname === 'set-kv') {
+            context = await env.OoOo.put("say", "Hello World!");
+            return new Response("Successful write", { status: 201 });
+        } else if (url.pathname === 'get-kv') {
+            const value = await env.OoOo.get("say");
+            return new Response(value, { status: 200 });
+        }
         return env.ASSETS.fetch(request);
     }
 }
