96ff8bfa74597793e4daa89f77243c2035f80a63
wenshibiao <wenshibiao@outlook.com>
2025-01-24 14:13:36 +0800

    edit _worker.js

--- _worker.js
+++ _worker.js
@@ -4,10 +4,10 @@ export default {
         if (url.pathname.startsWith('/api/')) {
 
             return new Response('Ok');
-        } else if (url.pathname.startsWith('set-kv')) {
+        } else if (url.pathname.startsWith('/set-kv')) {
             await env.OoOo.put("say", "Hello World!");
             return new Response("Successful write", { status: 201 });
-        } else if (url.pathname.startsWith('get-kv')) {
+        } else if (url.pathname.startsWith('/get-kv')) {
             const value = await env.OoOo.get("say");
             return new Response(value, { status: 200 });
         }
